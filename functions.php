89f86108ad9a6ef75e65ab7d2cb2024d1ed61ee7
Hex <Hex90@riseup.net>
2021-12-01 19:31:04 -0500

    Minor game interface improvements.

--- functions.php
+++ functions.php
@@ -24,6 +24,19 @@ function head ( array $args = [] ) {
 }
 
 /**
+ * Make random game text substitutions for various time stamps.
+ */
+function recentTimeRecursive ( string $input ) {
+    $count = null;
+    $input = preg_replace( '/__RECENT_TIME__/',
+        date( 'Y-m-d H:i',
+            strtotime('-' . random_int( 1, 59 ) . ' hour ' . random_int( 1, 59 ) . ' minutes ' . random_int( 1, 59 ). ' seconds')
+        ),
+    $input, 1, $count);
+    return (0 === $count) ? $input : recentTimeRecursive( $input );
+}
+
+/**
  * Performs some basic substitutions for text in the game.
  */
 function gameText ( string $text = '', array $challenge = [], string $user_flag = '' ) {
@@ -33,12 +46,18 @@ function gameText ( string $text = '', array $challenge = [], string $user_flag 
         "__CHALLENGE_PROMPT__" => $challenge['prompt'],
         "__CHALLENGE_TEXT__" => $challenge['text'],
         "__NOW__" => date( 'D M d H:i:s T Y' ),
+        "__REMOTE_ADDR__" => $_SERVER['REMOTE_ADDR'],
         "__USER_FLAG__" => $user_flag
     ];
     $search = array_keys( $static_substitution_map );
     $replace = array_values( $static_substitution_map );
     $text = str_replace( $search, $replace, str_replace( $search, $replace, $text ) );
-    return preg_replace_callback( '/__RANDOM_INT__/', function () {
+
+    // Dynamic replacements.
+    $text = recentTimeRecursive( $text );
+    $text = preg_replace_callback( '/__RANDOM_INT__/', function () {
         return random_int( 10, 99 );
     }, $text );
+
+    return $text;
 }
